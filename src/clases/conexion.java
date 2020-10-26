9156f0e91efb532161909e6f567f7aeb3aa6e384
fabiancho_194@hotmail.com <fabaiancho_194@hotmail.com>
2020-10-25 19:59:11 -0500

    primer punto de partida

---
+++ src/clases/conexion.java
@@ -0,0 +1,15 @@
+package clases;
+import java.sql.*;
+
+public class conexion {
+    //conexion Local
+    public static Connection conectar(){
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_ds", "root", "");
+            return cn;
+        } catch (SQLException e) {
+            System.out.println("Error en la conexion" + e);  
+        }       
+        return (null);
+    }
+}
